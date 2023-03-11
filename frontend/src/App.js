8b92da1566fcf778df9a3be0a45a02e5a4387a77
thanhtunggc92 <nguyentruongthanhtung@gmail.com>
2023-03-10 22:53:59 -0800

    render information of the product

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,27 @@
 import Header from './components/Header'
 import Footer from './components/Footer'
+// import {Route, BrowserRouter as Router  } from 'react-router-dom';
+import { BrowserRouter as Router, Route ,Routes} from 'react-router-dom'
 import {Container} from 'react-bootstrap'
 import HomeScreen from './screens/HomeScreens'
+import ProductScreen from './screens/ProductScreen';
+
 function App() {
   return (
-   
-    <div >
-    <Header />
-   <main className='py-5'>
-      <Container>
-          <HomeScreen />
-      </Container>
-   </main>
-    <Footer />
-    </div>
+    <Router>
+      <Header />
+      <main className='py-5'>
+        <Container>
+          <Routes>
+          <Route path='/' element={<HomeScreen />}  />
+          <Route path='/product/:id'   element={<ProductScreen />} />
+          </Routes>
+         
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
 }
 
-export default App ;
+export default App;
