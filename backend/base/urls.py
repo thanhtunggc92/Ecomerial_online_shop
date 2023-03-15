48212e20ad5765d54c5eed91e2573fffe82cacdd
thanhtunggc92 <nguyentruongthanhtung@gmail.com>
2023-03-15 10:28:03 -0700

    set user restriction for admin and normal user

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -4,8 +4,9 @@ from . import views
 
 urlpatterns =[
     path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('',views.getRoute,name= 'home'),
+
     path('products/',views.getProducts,name ='products'),
     path('users/profile',views.getUserProfile,name ='user-profile'),
+    path('users/',views.getUsers,name ='users'),
     path('products/<str:pk>/',views.getProduct,name ='product'),
 ]
